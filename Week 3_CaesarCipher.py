48b287e8e8159611e388da028330f0c7cc24e97a
wturner94 <williamj.turner94@gmail.com>
2023-07-26 20:33:35 -0400

    first draft of Caesar Cipher assignment, need to figure out how to convert list of letters to dictionary keys and recall/print them. also need to figure out the while loop.

---
+++ Week 3_CaesarCipher.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+caesarDict = {
+    "a": "v",
+    "b": "w",
+    "c": "x",
+    "d": "y",
+    "e": "z",
+    "f": "a",
+    "g": "b",
+    "h": "c",
+    "i": "d",
+    "j": "e",
+    "k": "f",
+    "l": "g",
+    "m": "h",
+    "n": "i",
+    "o": "j",
+    "p": "k",
+    "q": "l",
+    "r": "m",
+    "s": "n",
+    "t": "o",
+    "u": "p",
+    "v": "q",
+    "w": "r",
+    "x": "s",
+    "y": "t",
+    "z": "u",
+    
+            }
+
+sentence = input("Welcome to Caesar's Cipher program. Please enter a phrase to be encrypted: ")
+sentence = sentence.lower()
+letters = [*sentence]
+
+        
+#while True:
+    #if letters != caesarDict.keys():
+        #print("No numbers or special characters allowed!")
+    #else:
+        #break
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
